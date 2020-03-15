167228fb652c18e97bfb7b6bfce0051c02f5f943
Joe Paris <parisj@linnbenton.edu>
2020-03-15 16:55:39 -0700

    First commit, with errors

---
+++ mymath.py
@@ -0,0 +1,7 @@
+def add(x, y):
+    """Is this a docstring? Is this a docstring? Is this a docstring? Is this a docstring? Is this a docstring?"""
+    return x+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y
+
+
+def subtract(x, y):
+    return x/y
