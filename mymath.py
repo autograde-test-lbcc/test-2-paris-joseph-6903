8b3d1acb66533c0fccb1124e0677a14ac883670a
Joe Paris <parisj@linnbenton.edu>
2020-03-15 17:37:38 -0700

    Fix line length

--- mymath.py
+++ mymath.py
@@ -1,6 +1,6 @@
 def add(x, y):
-    """Is this a docstring? Is this a docstring? Is this a docstring? Is this a docstring? Is this a docstring?"""
-    return x+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y
+    """Is this a docstring?"""
+    return x+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y
 
 
 def subtract(x, y):
